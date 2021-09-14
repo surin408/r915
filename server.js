d6275383aa00c566d288a90e38d0dfaee976ed0d
surin408 <surin408@gmail.com>
2021-09-14 11:42:57 +0700

    server changed

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const ngApp = express();
 
-ngApp.use(express.static('./dist/angular-forms-validation'));
+ngApp.use(express.static('./dist/r915'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/r915/index.html'));
 });
 
 ngApp.listen(process.env.PORT || 8080);
